8f70ceade8caa9c25421bd7605c32992a855800e
IvanShevchenko135 <ivanshevchenko135@gmail.com>
2022-06-16 23:35:00 +0300

    gitignore build

--- build/lib/recognition/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('api/transcript/', views.Transript.as_view()),
-    path('api/prediction/', views.Prediction.as_view()),
-    path('api/start/', views.StartRecording.as_view()),
-]
